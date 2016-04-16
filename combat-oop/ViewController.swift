d885b00eb661fa3cbc54ca0b5d9e9f389b2520ba
Amadeu Andrade <amadeuandrade@Amadeus-MBP.home>
2016-04-16 14:49:52 +0100

    Added final layout

--- combat-oop/ViewController.swift
+++ combat-oop/ViewController.swift
@@ -10,6 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var logoImg: UIImageView!
+    @IBOutlet weak var player1nameLbl: UITextField!
+    @IBOutlet weak var selectCharLbl: UILabel!
+    @IBOutlet weak var player1SelectTrollImg: UIButton!
+    @IBOutlet weak var player1SelectSoldierImg: UIButton!
+    @IBOutlet weak var player2nameLbl: UITextField!
+    @IBOutlet weak var selectChar2Lbl: UILabel!
+    @IBOutlet weak var player2SelectTrollImg: UIButton!
+    @IBOutlet weak var player2SelectSoldierImg: UIButton!
+    @IBOutlet weak var groundImg: UIImageView!
+    @IBOutlet weak var textHolderLbl: UIImageView!
+    @IBOutlet weak var beginBtn: UIButton!
+    @IBOutlet weak var player1AttackBtn: UIImageView!
+    @IBOutlet weak var player2AttackBtn: UIImageView!
+    @IBOutlet weak var player1HpLbl: UILabel!
+    @IBOutlet weak var player2HpLbl: UILabel!
+    @IBOutlet weak var player1Img: UIImageView!
+    @IBOutlet weak var player2Img: UIImageView!
+    
+    @IBAction func onPlayer1SelectSoldier(sender: AnyObject) {
+    }
+    
+    @IBAction func onPlayer1SelectTroll(sender: AnyObject) {
+    }
+    
+    @IBAction func onPlayer2SelectSoldier(sender: AnyObject) {
+    }
+    @IBAction func onPlayer2SelectTroll(sender: AnyObject) {
+    }
+    
+    @IBAction func onBeginPressed(sender: AnyObject) {
+    }
+    
+
+    
+    
+    
+    
+    
     
     
     override func viewDidLoad() {
